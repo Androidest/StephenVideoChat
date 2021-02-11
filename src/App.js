38fe699b5b578eda9cf6504f368cd7c57cb910d5
Stephen_Leong <junhua.liang@banana.games>
2021-02-11 17:10:57 +0800

    change ip
    Signed-off-by: Stephen_Leong <junhua.liang@banana.games>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 	const [response, setResponse] = useState("");
 
 	useEffect(() => {
-		const socket = io('https://39.98.118.62:443');
+		const socket = io('39.98.118.62');
 		socket.on("FromAPI", data => {
 			setResponse(data);
 			socket.emit("test", 'FUCK!');
