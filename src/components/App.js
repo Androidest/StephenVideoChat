c7265f15880964bbdaf182854c7440f4e97b53e0
Stephen_Leong <junhua.liang@banana.games>
2021-02-15 14:24:18 +0800

    added firebase & styled-component & _init_ folder

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import firebase from 'modules/database'
+import * as tf from '@tensorflow/tfjs'
+import * as WebCam from 'react-webcam' 
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  	return (
+ 		<AppContainer>
+    	</AppContainer>
+	);
+}
+
+const AppContainer = styled.div`
+	background-color: #faf8ef;
+	width: 100%;
+	height: 100%;
+`;
+
+export default App;
---
+++ src/modules/database.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/analytics";
+
+{
+    const firebaseConfig = {
+        apiKey: "AIzaSyByUAid1ySJpc7RGmLGdtsuceuFLJcr32A",
+        authDomain: "react-tensor-app.firebaseapp.com",
+        projectId: "react-tensor-app",
+        storageBucket: "react-tensor-app.appspot.com",
+        messagingSenderId: "710084648290",
+        appId: "1:710084648290:web:f9b770ead99b5f0c8aa03b",
+        measurementId: "G-L446FGJC6L"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+}
+
+export default firebase;
