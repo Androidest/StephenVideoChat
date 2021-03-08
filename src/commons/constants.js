ce643bafa310ac852ce2fc113a4c33493352cc8c
Stephen_Leong <junhua.liang@banana.games>
2021-03-08 18:42:14 +0800

    edit stun & turn server config

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -18,12 +18,11 @@ export const peerConfig = {
         {url:'stun:stun.voxgratia.org'},
         {url:'stun:stun.xten.com'},
         {url:'stun:stun01.sipphone.com'},
-        {url:'stun:stun1.l.google.com:19302'},
-        {url:'stun:stun1.l.google.com:19302'},
-        {url:'stun:stun1.l.google.com:19302'},
+        {url:'stun:stun.l.google.com:19302'},
         {url:'stun:stun1.l.google.com:19302'},
         {url:'stun:stun2.l.google.com:19302'},
-        { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
+        {url:'stun:stun3.l.google.com:19302'},
+        {url:'stun:stun4.l.google.com:19302'}
     ], 
     sdpSemantics: 'unified-plan' 
 };
