2239e49585984a630772bd18a56c4525b00da87a
Stephen_Leong <junhua.liang@banana.games>
2021-03-08 17:58:38 +0800

    update trun server & stun servers config

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -6,11 +6,22 @@ export const peerClientID = 'StphClientID_'+v4().replaceAll('-','_'); //将减�
 export const peerServerID = 'Alan-Stephen-Leong-Song_ServerID_mJygpKissLS4vM';
 export const peerConfig = { 
     iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' },
+        {url:'stun:stun1.l.google.com:19302'},
+        {url:'stun:stun2.l.google.com:19302'},
         {
             url: 'turn:numb.viagenie.ca',
             credential: 'muazkh',
             username: 'webrtc@live.com'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
         }
     ], 
     sdpSemantics: 'unified-plan' 
